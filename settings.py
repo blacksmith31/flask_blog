b10d1e831a2c5109439c9a7743da05e585641551
Jameson Marshall <jamesonhmarshall@gmail.com>
2017-11-06 23:26:25 +0000

    updated with registration form, base.html, form helper macro

--- settings.py
+++ settings.py
@@ -1,3 +1,10 @@
 SECRET_KEY = 'you-will-never-guess'
 DEBUG = True
 
+DB_HOST = 'jamesonhm.mysql.pythonanywhere-services.com'
+DB_USERNAME = 'jamesonhm'
+DB_PASSWORD = 'learnFlask1'
+DB_NAME = 'jamesonhm$blog'
+DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME)
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = True
